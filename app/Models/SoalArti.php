c88d3d8cd5c4d0b6642f2da95aa18f233917258d
rRasyad <38708475+rRasyad@users.noreply.github.com>
2023-02-27 10:47:04 +0700

    feat [R] add SoalArti->Model,Factory,Migration,Seeder

---
+++ app/Models/SoalArti.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel\BasePivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class SoalArti extends BasePivot
+{
+    use HasFactory;
+}
