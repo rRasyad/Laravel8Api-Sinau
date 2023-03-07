0029f9cd0d7a908f76b7b4f824887b2d52a194ab
rRasyad <38708475+rRasyad@users.noreply.github.com>
2023-03-07 15:41:30 +0700

    fix [R] simplified Streak & change dy -> yesterday, fix route spelling

--- routes/api.php
+++ routes/api.php
@@ -69,7 +69,7 @@ Route::group(['middleware' => ['auth:sanctum', 'ability:user,admin', 'after.midd
     Route::get('mapel', [ContentController::class, 'mapel']); //? Ok
     Route::get('quest', [QuestController::class, 'quest']); //? Ok
     // Route::get('inisiasi-session/{id}', [ContentController::class, 'inisiasi']);
-    Route::get('initiation-session', [ContentController::class, 'initiation']); //? Ok
+    Route::get('initial-session', [ContentController::class, 'initiation']); //? Ok
     Route::get('next-soal', [ContentController::class, 'nextSession']); //? Ok
 });
 
